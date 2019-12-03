1f63b4cf257c9a3664ec022dc13580cb9378afbd
linjiang <linj2@illinois.edu>
2019-12-03 15:08:19 -0600

    commit test

--- app/src/main/java/com/example/travelovertheworld_xing_lin/India_1.java
+++ app/src/main/java/com/example/travelovertheworld_xing_lin/India_1.java
@@ -12,7 +12,7 @@ public class India_1 extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.indian);
+        setContentView(R.layout.indian_new);
 
         Button backbutton = (Button) findViewById(R.id.back_button);
         backbutton.setOnClickListener(new View.OnClickListener() {

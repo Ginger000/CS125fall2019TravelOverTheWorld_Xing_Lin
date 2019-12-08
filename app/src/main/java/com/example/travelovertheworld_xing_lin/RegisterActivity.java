e5b3a0bb0d220e0263385ecdf8e1af880a65b0c0
linjiang <linj2@illinois.edu>
2019-12-07 21:57:05 -0600

    commit test

---
+++ app/src/main/java/com/example/travelovertheworld_xing_lin/RegisterActivity.java
@@ -0,0 +1,14 @@
+package com.example.travelovertheworld_xing_lin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
